5f0d0fc406080158326a15e7bf436552623dbc40
IlonaSofianidi <sofianidi.ilona@gmail.com>
2018-10-26 21:32:22 +0300

    Add audio files ,change word categories to play this files.

--- app/src/main/java/com/example/android/miwok/ColorsActivity.java
+++ app/src/main/java/com/example/android/miwok/ColorsActivity.java
@@ -1,7 +1,10 @@
 package com.example.android.miwok;
 
 import android.app.Activity;
+import android.media.MediaPlayer;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ListView;
 
 import java.util.ArrayList;
@@ -14,18 +17,26 @@ public class ColorsActivity extends Activity {
         setContentView(R.layout.word_list);
 
         ArrayList<Word> words = new ArrayList<Word>();
-        words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red));
-        words.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_dusty_yellow));
-        words.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow));
-        words.add(new Word("green", "chokokki", R.drawable.color_green));
-        words.add(new Word("brown", "ṭakaakki", R.drawable.color_brown));
-        words.add(new Word("gray", "ṭopoppi", R.drawable.color_gray));
-        words.add(new Word("black", "kululli", R.drawable.color_black));
-        words.add(new Word("white", "kelelli", R.drawable.color_white));
+        words.add(new Word("red", "weṭeṭṭi", R.drawable.color_red, R.raw.color_red));
+        words.add(new Word("mustard yellow", "chiwiiṭә", R.drawable.color_dusty_yellow, R.raw.color_mustard_yellow));
+        words.add(new Word("dusty yellow", "ṭopiisә", R.drawable.color_dusty_yellow, R.raw.color_dusty_yellow));
+        words.add(new Word("green", "chokokki", R.drawable.color_green, R.raw.color_green));
+        words.add(new Word("brown", "ṭakaakki", R.drawable.color_brown, R.raw.color_brown));
+        words.add(new Word("gray", "ṭopoppi", R.drawable.color_gray, R.raw.color_gray));
+        words.add(new Word("black", "kululli", R.drawable.color_black, R.raw.color_black));
+        words.add(new Word("white", "kelelli", R.drawable.color_white, R.raw.color_white));
 
         WordsAdapter wordsAdapter = new WordsAdapter(this, R.layout.list_item, words, R.color.category_colors);
         ListView listView = (ListView)findViewById(R.id.list_words);
         listView.setAdapter(wordsAdapter);
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+                Word word = (Word) adapterView.getItemAtPosition(i);
+                MediaPlayer mediaPlayer = MediaPlayer.create(ColorsActivity.this, word.getAudio_resource_id());
+                mediaPlayer.start();
+            }
+        });
 
     }
 }
--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -8,10 +8,23 @@ public class Word {
     private int imageResourceId = NO_IMAGE_PROVIDED;
     private int image_resource_id;
 
-    public Word(String english_translation, String miwok_translation, int image_resource_id) {
+    private int audio_resource_id;
+
+    public Word(String english_translation, String miwok_translation, int image_resource_id, int audio_resource_id) {
         this.english_translation = english_translation;
         this.miwok_translation = miwok_translation;
         this.image_resource_id = image_resource_id;
+        this.audio_resource_id = audio_resource_id;
+    }
+
+    public Word(String english_translation, String miwok_translation, int audio_resource_id) {
+        this.english_translation = english_translation;
+        this.miwok_translation = miwok_translation;
+        this.audio_resource_id = audio_resource_id;
+    }
+
+    public int getAudio_resource_id() {
+        return audio_resource_id;
     }
 
     public int getImage_resource_id() {
@@ -22,9 +35,8 @@ public class Word {
         this.image_resource_id = image_resource_id;
     }
 
-    public Word(String english_translation, String miwok_translation) {
-        this.english_translation = english_translation;
-        this.miwok_translation = miwok_translation;
+    public void setAudio_resource_id(int audio_resource_id) {
+        this.audio_resource_id = audio_resource_id;
     }
 
     public String getEnglish_translation() {
